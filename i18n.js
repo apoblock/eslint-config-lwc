dc66a848f867dd01e4596e0b5f9e2bf627ad8a52
Andrew Poblocki <apoblocki@salesforce.com>
2020-11-24 10:37:38 -0800

    feat: adding i18n rules

---
+++ i18n.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+'use strict';
+
+module.exports = {
+    plugins: ['@lwc/eslint-plugin-lwc'],
+
+    rules: {
+        // I18N Rules
+        '@lwc/lwc/no-aura-localization-service': 'warn',
+        '@lwc/lwc/no-moment': 'warn',
+        '@lwc/lwc/prefer-localizer': 'warn',
+    },
+};
